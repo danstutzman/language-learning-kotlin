4ea59dcb4e1fff86398fbc8b21a1181c9f9eb186
Daniel Stutzman <dtstutz@gmail.com>
2017-08-16 23:10:18 -0600

    Put seeds in a tight array for convenience

--- src/main/resources/db/migration/V1__create_actions.sql
+++ src/main/resources/db/migration/V1__create_actions.sql
@@ -12,30 +12,33 @@ CREATE UNIQUE INDEX idx_actions_action_id on actions(action_id);
 
 CREATE INDEX idx_actions_created_at_millis on actions(created_at_millis);
 
-INSERT INTO actions (
-  action_id,
-  type,
-  created_at_millis,
-  synced_at,
-  card_json
-) VALUES (
-  10,
-  'ADD_CARD',
-  EXTRACT(EPOCH FROM NOW()) * 1000,
-  NOW(),
-  '{"type":"EsN", "gender":"M", "es":"libro", "en":"book"}'
-);
-
-INSERT INTO actions (
-  action_id,
-  type,
-  created_at_millis,
-  synced_at,
-  card_json
-) VALUES (
-  20,
-  'ADD_CARD',
-  EXTRACT(EPOCH FROM NOW()) * 1000,
-  NOW(),
-  '{"type":"EsN", "gender":"F", "es":"pluma", "en":"pen"}'
-);
+DO
+$do$
+DECLARE
+	m   varchar[];
+	arr varchar[] := array[
+    ['10','M','libro','book'],
+    ['20','F','pluma','pen']
+  ];
+BEGIN
+	FOREACH m SLICE 1 IN ARRAY arr
+	LOOP
+		INSERT INTO actions (
+			action_id,
+			type,
+			created_at_millis,
+			synced_at,
+			card_json
+		) VALUES (
+			CAST(m[1] AS INT),
+			'ADD_CARD',
+			EXTRACT(EPOCH FROM NOW()) * 1000,
+			NOW(),
+			'{"type":"EsN",' ||
+      ' "gender":"' || m[2] || '",' ||
+      ' "es":"' || m[3] || '",' ||
+      ' "en":"' || m[4] || '"}'
+		);
+   END LOOP;
+END
+$do$
