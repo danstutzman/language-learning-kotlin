94b8875b7bb48fb63c86c232ea4fb302cd6d9b1f
Daniel Stutzman <dtstutz@gmail.com>
2017-08-17 17:05:46 -0600

    Add a bunch of body parts to vocab

--- src/main/resources/db/migration/V1__create_actions.sql
+++ src/main/resources/db/migration/V1__create_actions.sql
@@ -18,9 +18,38 @@ $do$
 DECLARE
 	m   varchar[];
 	arr varchar[] := array[
-    ['10','M','libro','book'],
-    ['20','F','pluma','pen']
+    ['skin','piel','F']
+  , ['arm','brazo','M']
+  , ['leg','pierna','F']
+  , ['heart','corazón','M']
+  , ['stomach','estómago','M']
+  , ['eye','ojo','M']
+  , ['nose','nariz','F']
+  , ['mouth','boca','F']
+  , ['ear','oreja','F']
+  , ['face','cara','F']
+  , ['neck','cuello','M']
+  , ['finger','dedo','M']
+  , ['foot','pie','M']
+  , ['thigh','muslo','M']
+  , ['ankle','tobillo','M']
+  , ['elbow','codo','M']
+  , ['wrist','muñeca','F']
+  , ['buttocks','nalgas','F']
+  , ['body','cuerpo','M']
+  , ['hair','pelo','M']
+  , ['tooth','diente','M']
+  , ['hand','mano','F']
+  , ['back','espalda','F']
+  , ['hip','cadera','F']
+  , ['jaw','mandibula','F']
+  , ['hombro','shoulder','M']
+  , ['thumb','pulgar','M']
+  , ['tongue','lengua','F']
+  , ['throat','garganta','F']
   ];
+DECLARE
+  action_id int8 := 10;
 BEGIN
 	FOREACH m SLICE 1 IN ARRAY arr
 	LOOP
@@ -31,16 +60,17 @@ BEGIN
 			synced_at,
 			card_json
 		) VALUES (
-			CAST(m[1] AS INT),
+      action_id,
 			'ADD_CARD',
 			EXTRACT(EPOCH FROM NOW()) * 1000,
 			NOW(),
-			'{"type":"EsN",' ||
-      ' "cardId":' || m[1] || ',' ||
-      ' "gender":"' || m[2] || '",' ||
-      ' "es":"' || m[3] || '",' ||
-      ' "en":"' || m[4] || '"}'
+			'{"type":"EsN",'                   ||
+      ' "cardId":'  || action_id || ','  ||
+      ' "en":"'     || m[1]      || '",' ||
+      ' "es":"'     || m[2]      || '",' ||
+      ' "gender":"' || m[3]      || '"}'
 		);
+    action_id := action_id + 10;
    END LOOP;
 END
 $do$
