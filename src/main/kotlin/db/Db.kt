c42fb613af380626ae170fd080698690ae628c34
Daniel Stutzman <dtstutz@gmail.com>
2018-09-05 20:57:12 -0600

    Remove unused imports

--- src/main/kotlin/db/Db.kt
+++ src/main/kotlin/db/Db.kt
@@ -1,17 +1,6 @@
 package com.danstutzman.db
 
-import com.google.gson.Gson
-import com.google.gson.JsonObject
-import com.google.gson.JsonParser
-import org.jooq.DSLContext
-import org.jooq.SQLDialect
-import org.jooq.generated.tables.Cards.CARDS
-import org.jooq.generated.tables.CardEmbeddings.CARD_EMBEDDINGS
-import org.jooq.generated.tables.Leafs.LEAFS
-import org.jooq.generated.tables.Paragraphs.PARAGRAPHS
-import org.jooq.impl.DSL
 import java.sql.Connection
-import java.sql.Timestamp
 
 class Db(
   private val conn: Connection
