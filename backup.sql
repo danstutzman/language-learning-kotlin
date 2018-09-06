aa3e9bd93bb3ecdfee2e9d13616e6800a8936ec6
Daniel Stutzman <dtstutz@gmail.com>
2018-09-05 21:06:07 -0600

    Add vowels for Arabic word for God

--- backup.sql
+++ backup.sql
@@ -1864,11 +1864,11 @@ COPY leafs (leaf_id, leaf_type, es_mixed, en, en_disambiguation, en_plural, en_p
 647	ArNonverb	\N	book	\N	\N	\N	\N	\N	\N	2018-09-05 18:39:15.038859-06	\N	\N	kitAb
 652	ArNonverb	\N	goodbye	\N	\N	\N	\N	\N	\N	2018-09-05 20:43:56.066319-06	\N	\N	maEa Als~alAmp
 653	ArNonverb	\N	you drink	\N	\N	\N	\N	\N	\N	2018-09-05 20:47:47.900079-06	\N	\N	ta$rab
-654	ArNonverb	\N	God	\N	\N	\N	\N	\N	\N	2018-09-05 20:50:29.260426-06	\N	\N	Allh
 648	ArNonverb	\N	Ramadan	\N	\N	\N	\N	\N	\N	2018-09-05 19:56:46.940322-06	\N	\N	ramaDAn
 649	ArNonverb	\N	sugar	\N	\N	\N	\N	\N	\N	2018-09-05 20:02:33.713172-06	\N	\N	sukar
 650	ArNonverb	\N	thirsty	\N	\N	\N	\N	\N	\N	2018-09-05 20:06:25.526182-06	\N	\N	EaTo$An
 651	ArNonverb	\N	baker	\N	\N	\N	\N	\N	\N	2018-09-05 20:37:09.541727-06	\N	\N	xab~Az
+655	ArNonverb	\N	God	\N	\N	\N	\N	\N	\N	2018-09-05 21:02:40.168287-06	\N	\N	{ll~`h
 \.
 
 
@@ -1876,7 +1876,7 @@ COPY leafs (leaf_id, leaf_type, es_mixed, en, en_disambiguation, en_plural, en_p
 -- Name: leafs_leaf_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
 
-SELECT pg_catalog.setval('leafs_leaf_id_seq', 654, true);
+SELECT pg_catalog.setval('leafs_leaf_id_seq', 655, true);
 
 
 --
