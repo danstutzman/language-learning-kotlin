8c9d5f100a871620422c5d64ebd054dc8f808fb0
Daniel Stutzman <dtstutz@gmail.com>
2018-09-26 14:27:32 -0600

    Add PronounceAtomContext2

---
+++ src/main/kotlin/arabic/PronounceAtomContext2.kt
@@ -0,0 +1,74 @@
+package com.danstutzman.arabic
+
+val TO_IPA = mapOf(
+  "a" to "a",
+  "aa" to "æ",
+  "i" to "i",
+  "ii" to "iː",
+  "u" to "u",
+  "uu" to "uː",
+  "'" to "ʔ", // glottal stop
+  "D" to "dˤ",
+  "H" to "ħ",
+  "S" to "sˤ",
+  "T" to "tˤ",
+  "Z" to "zˤ",
+  "b" to "b",
+  "c" to "ʕ", // guttural voiced h
+  "d" to "d",
+  "dh" to "ð", // "these"
+  "gh" to "ɣ", // French "parler"
+  "f" to "f",
+  "h" to "h",
+  "j" to "dʒ", // "jam"
+  "k" to "k",
+  "l" to "l",
+  "m" to "m",
+  "n" to "n",
+  "q" to "q", // "scar"
+  "r" to "r", // tapped
+  "s" to "s",
+  "sh" to "ʃ",
+  "t" to "t",
+  "th" to "θ", // "three"
+  "w" to "w",
+  "x" to "x", // "Bach"
+  "y" to "j",
+  "z" to "z")
+
+fun pronounceAtomContext2(atomContext2: AtomContext2): String {
+  val atom = atomContext2.atom
+  val left = atomContext2.left
+  val right = atomContext2.right
+  val right2 = atomContext2.right2
+
+  if (atom == "a" || atom == "aa") {
+    if (left == "D" || right == "D" || right2 == "D") return "ɑ"
+    if (left == "S" || right == "S" || right2 == "S") return "ɑ"
+    if (left == "T" || right == "T" || right2 == "T") return "ɑ"
+    if (left == "Z" || right == "Z" || right2 == "Z") return "ɑ"
+    if (left == "q" || right == "q" || right2 == "q") return "ɑ"
+    return "æ"
+  } else if (atom == "i") {
+    if (left == "D" || right == "D" || right2 == "D") return "ɘ"
+    if (left == "S" || right == "S" || right2 == "S") return "ɘ"
+    if (left == "T" || right == "T" || right2 == "T") return "ɘ"
+    if (left == "Z" || right == "Z" || right2 == "Z") return "ɘ"
+    return "ɪ"
+  } else if (atom == "u") {
+    if (left == "y") { return "u" }
+    if (left == "D" || right == "D" || right2 == "D") return "ɔ"
+    if (left == "S" || right == "S" || right2 == "S") return "ɔ"
+    if (left == "T" || right == "T" || right2 == "T") return "ɔ"
+    if (left == "Z" || right == "Z" || right2 == "Z") return "ɔ"
+    return "o"
+  } else if (atom == "e") {
+    return ""
+  } else {
+    val ipa = TO_IPA[atom]
+    if (ipa == null) {
+      throw RuntimeException("Unknown pronunciation for atom '$atom'")
+    }
+    return ipa
+  }
+}
---
+++ src/test/kotlin/PronounceAtomContext2Test.kt
@@ -0,0 +1,65 @@
+package com.danstutzman.arabic
+
+import com.danstutzman.arabic.addContextToAtoms
+import com.danstutzman.arabic.pronounceAtomContext2
+import com.danstutzman.arabic.splitQalamIntoAtoms
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
+import org.opentest4j.AssertionFailedError
+
+fun pronounce(qalam: String): String {
+  val atoms = splitQalamIntoAtoms(qalam)
+  val contexts1 = atoms.map { atom ->
+    AtomContext1(
+      atom=atom,
+      beginPunctuation="",
+      endPunctuation="",
+      endsMorpheme=false)
+  }
+  val contexts2 = addContextToAtoms(contexts1)
+  val pronunciations = contexts2.map { context ->
+    pronounceAtomContext2(context) +
+    (if (context.endsSyllable) "." else "")
+  }
+  return pronunciations.joinToString("").replace("\\.$".toRegex(), "")
+}
+
+// Examples from https://www.lebanesearabicinstitute.com/arabic-alphabet/
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class PronounceAtomContext2Test {
+  @Test
+  fun testAdjustsVowelsBasedOnSurroundingConsonants() {
+    assertEquals("sˤɑ.ʕiːd", pronounce("Saciid"))
+    assertEquals("sæ.ʕiːd", pronounce("saciid"))
+    assertEquals("ħɑ.sˤɘd", pronounce("HaaSid"))
+    assertEquals("ħæ.sɪd", pronounce("Haasid"))
+    assertEquals("ħæ.rɑsˤ", pronounce("HaraS"))
+    assertEquals("ħæ.ræs", pronounce("Haras"))
+    assertEquals("tˤɑʔ", pronounce("Taa'"))
+    assertEquals("tæʔ", pronounce("taa'"))
+    assertEquals("ju.rɑtˤ.tˤɘb", pronounce("yuraTTib"))
+    assertEquals("ju.ræt.tɪb", pronounce("yurattib"))
+    assertEquals("ħɑtˤ", pronounce("HaaT"))
+    assertEquals("ħæt", pronounce("Haat"))
+    assertEquals("zˤɑ.liːl", pronounce("Zaliil"))
+    assertEquals("ðæ.liːl", pronounce("dhaliil"))
+    assertEquals("mɑħ.zˤuːr", pronounce("maHZuur"))
+    assertEquals("mæħ.ðuːr", pronounce("maHdhuur"))
+    assertEquals("bɑzˤzˤ", pronounce("baZZ"))
+    assertEquals("bæðð", pronounce("badhdh"))
+    assertEquals("dˤɑrr", pronounce("Darr"))
+    assertEquals("dærr", pronounce("darr"))
+    assertEquals("tæ.ħɑdˤ.dˤɔr", pronounce("taHaDDur"))
+    assertEquals("tæ.ħæd.dor", pronounce("taHaddur"))
+    assertEquals("rɔ.dˤuːdˤ", pronounce("ruDuuD"))
+    assertEquals("ro.duːd", pronounce("ruduud"))
+    assertEquals("kælb", pronounce("kalb"))
+    assertEquals("qɑlb", pronounce("qalb"))
+    assertEquals("tæk.riːr", pronounce("takriir"))
+    assertEquals("tɑq.riːr", pronounce("taqriir"))
+    assertEquals("ħækk", pronounce("Hakk"))
+    assertEquals("ħɑqq", pronounce("Haqq"))
+  }
+}
